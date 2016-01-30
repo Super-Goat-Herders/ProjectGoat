3dd244edd91e1bc9a06e0d13e3c7a24c21011838
Garrett Baski <garbaski@cs.du.edu>
2016-01-30 09:38:27 -0700

    Whatever here you go

---
+++ GoatHerder/Assets/ShootAttack.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShootAttack : MonoBehaviour {
+    public float timer;
+    public Object arrows;
+    public float currentCheck;
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+        timer = 0;
+        currentCheck = ((Random.value * 2) + 5);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        Debug.Log(timer);
+        transform.LookAt(target);
+        if (timer > currentCheck)
+        {
+            Object MyArrow = Instantiate(arrows, transform.position, transform.rotation);
+        }
+	}
+}
