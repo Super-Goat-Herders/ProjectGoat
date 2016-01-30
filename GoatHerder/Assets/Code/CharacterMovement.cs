172cd8c95f16e312bc64dfdbb251ccb1d02a18ee
Garrett Baski <garbaski@cs.du.edu>
2016-01-29 21:44:15 -0700

    Start to engine
    Can move a box

---
+++ GoatHerder/Assets/Code/CharacterMovement.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterMovement : MonoBehaviour {
+    float speed = 2f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        transform.position += move * speed * Time.deltaTime;
+        //Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y + 1f, -10f);
+	}
+}
