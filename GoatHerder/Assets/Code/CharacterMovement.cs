cee87195f6e79fa0545b337fc60af8f559a738a3
Garrett Baski <garbaski@cs.du.edu>
2016-01-30 15:56:47 -0700

    Base shepard is in

--- GoatHerder/Assets/Code/CharacterMovement.cs
+++ GoatHerder/Assets/Code/CharacterMovement.cs
@@ -3,15 +3,50 @@ using System.Collections;
 
 public class CharacterMovement : MonoBehaviour {
     float speed = 2f;
+    Animator playerAnimator;
 
 	// Use this for initialization
 	void Start () {
+        playerAnimator = GetComponent<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
         transform.position += move * speed * Time.deltaTime;
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            if (playerAnimator.GetBool("Stopped") == true)
+            {
+                playerAnimator.SetBool("Stopped", false);
+            }
+        }
+        if (Input.GetAxis("Vertical") != 0)
+        {
+            if (playerAnimator.GetBool("Stopped") == true)
+            {
+                playerAnimator.SetBool("Stopped", false);
+            }
+
+            if (Input.GetAxis("Vertical") > 0)
+            {
+                playerAnimator.SetBool("Backward", true);
+                playerAnimator.SetBool("Forward", false);
+            }
+            if (Input.GetAxis("Vertical") < 0)
+            {
+                playerAnimator.SetBool("Forward", true);
+                playerAnimator.SetBool("Backward", false);
+            }
+        }
+        else
+        {
+            playerAnimator.SetBool("Forward", false);
+            playerAnimator.SetBool("Backward", false);
+            playerAnimator.SetBool("Stopped", true);
+        }
+
+
         //Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y + 1f, -10f);
 	}
 
