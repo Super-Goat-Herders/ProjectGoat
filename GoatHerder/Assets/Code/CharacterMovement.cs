12495e71ba32ce520c0c26715303463230156b16
Paul Heinen <pheinenjr@gmail.com>
2016-01-30 13:03:05 -0700

    Game engine in all its untested glory

--- GoatHerder/Assets/Code/CharacterMovement.cs
+++ GoatHerder/Assets/Code/CharacterMovement.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class CharacterMovement : MonoBehaviour {
     float speed = 2f;
-
+    //bool isMoving = false;
 	// Use this for initialization
 	void Start () {
 	}
@@ -12,6 +12,6 @@ public class CharacterMovement : MonoBehaviour {
 	void Update () {
         var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
         transform.position += move * speed * Time.deltaTime;
-        //Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y + 1f, -10f);
+      //  Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y + 1f, -10f);
 	}
 }
