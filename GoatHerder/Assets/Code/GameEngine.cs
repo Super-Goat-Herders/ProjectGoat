9529076212b5e7fa43ac0aa9785e14dc564f8e7c
Paul Heinen <pheinenjr@gmail.com>
2016-01-30 13:16:25 -0700

    Game engine

--- GoatHerder/Assets/Code/GameEngine.cs
+++ GoatHerder/Assets/Code/GameEngine.cs
@@ -1,7 +1,198 @@
 using UnityEngine;
+using System.Collections;
 using System;
 
-
+//Game Engine
+public class GameEngine : MonoBehavior {
+    public enum TileType { Wall, Floor }
+    //Room Size
+    public int columns = 100;
+    public int rows = 100;
+    //Range of rooms available per level
+    public IntRange numRooms = new IntRange(15, 20);
+    
+    //Room Size settings
+    public IntRange roomWidth = new IntRange(3, 10);
+    public IntRange roomHeight = new IntRange(3, 10);
+    
+    //Corridor length
+    public IntRange corridorLength = new IntRange(6, 10);
+    
+    //Prefab Objects
+    public GameObject[] floorTiles;
+    public GameObject[] wallTiles;
+    public GameObject[] outerWalls;
+    public GameObject[] NPCs;
+    //....
+    
+    public GameObject player;
+    
+    //Level floor tiles
+    private TileType[][] tiles;
+    //Rooms in the level
+    private Room[] rooms;
+    //Cooridors in the level
+    private Corridor[] corridors;
+    //Container
+    private GameObject levelHolder;
+    
+    //Level Initialization 
+    private void Start() 
+    {
+        levelHolder = new GameObject("Level_1_Holder");
+        SetupTilesArray();
+        CreateRoomsAndCorridors();
+        InstantiateTiles ();
+        InstantiateOuterWalls ();   
+    }
+    
+    void setupTiles()
+    {
+        //needed for level tile grid width
+        tiles = new TileType[columns][];
+        for (int i = 0; i <= tiles.Length; ++i)
+        {
+           //needed for level tile grid height
+           tiles[i] = new TileType[rows];
+        }
+    }
+    void generateRoomsAndCorridors()
+    {
+        //Random number of rooms
+        rooms = new Room[numRooms.Random];
+        //And now we apply some pigeon hole principle for the number of corridors
+        corridors = new Corridor[rooms.Length - 1];
+        
+        //First room of the level
+        rooms[0] = new Room();
+        rooms[0].SetupRoom(roomWidth, roomHeight, columns, rows);
+        corridors[0] = new Corridor();
+        corridors[0].SetupCorridor(rooms[0], corridorLength, roomWidth, roomHeight, columns, rows, true);
+        
+        //generating the rest of the rooms and corridors on the level
+        for(int i = 1; i < rooms.Length; ++i)
+        {
+            rooms[i] = new Room();
+            //Connect room i to corridor i - 1
+            rooms[i].SetupRoom(roomWidth, roomHeight, columns, rows, corridors[i - 1]);
+            
+            //And add a corridor to the new room
+            if (i < corridorLength)
+            {
+                corridors[i] = new Corridor();
+                corridors[i].SetupCorridor(rooms[i], corridorLength, roomWidth, roomHeight, columns, rows, false);    
+            }
+            if(i == rooms.Length *.5f)
+            {
+                Vector3 playerPos = new Vector3 (rooms[i].xPos, rooms[i].yPos, 0);
+                Instantiate(player, playerPos, Quaternion.identity); //??? No idea what this does
+            }
+        }
+    }
+    //tripple nested for loops ftw!!!
+    void SetTileValuesForRooms()
+    {
+        for (int i = 0; i < rooms.Length; ++i)
+        {
+            Room currentRoom = rooms[i];
+            //go through the currentRooms width
+            for (int j = 0; j < currentRoom.roomWidth; ++j)
+            {
+                int xCoord = currentRoom.xPos + j;
+                //for each horizontal tile, go up vertically through the room's height
+                for (int k = 0; k < currentRoom.roomHeight; ++k)
+                {
+                    int yCoord = currentRoom.yPos + k;
+                    tiles[xCoord][yCoord] = TileType.Floor;
+                }
+            }
+        }
+    }
+    void SetTileValuesForCorridors()
+    {
+        for (int i = 0; i < corridors.Length; ++i)
+        {
+            Corridor currentCorridor = corridors[i];
+            //go through the currentCorridors length
+            for(int j = 0; j < currentCorridor.corridorLength; ++j)
+            {
+                int xCoord = currentCorridor.xStartPosition;
+                int yCoord = currentCorridor.yStartPosition;
+              
+                //more direction stuff
+                switch (currentCorridor.direction)
+                {
+                    case Direction.North: yCoord += j; break;
+                    case Direction.East: xCoord += j; break;
+                    case Direction.South: yCoord -= j; break;
+                    case Direction.West: xCoord -= j; break;
+                }
+                tiles[xCoord][yCoord] = TileType.Floor;
+            }
+        }
+    }
+    
+    void InstantiateTiles()
+    {
+        //Level floor tiles
+        for (int i = 0; i < tiles.Length; ++i)
+        {
+            for (int j = 0; j < tiles[i].Length; ++j)
+            {
+                InstantiateFromArray(floorTiles, i, j);
+                
+                //Tile Types
+                
+                //Wall
+                if (tiles[i][j] == TileType.Wall) InstantiateFromArray(wallTiles, i, j);
+            }
+        }
+    }
+    void InstantiateOuterWalls()
+    {
+        //Place 1 unit outside the Level Grid
+        float leftEdgeX = -1f;
+        float rightEdgeX = columns + 0f;
+        float bottomEdgeY = -1f;
+        float topEdgeY = rows + 0f;
+        
+        InstantiateVerticalOuterWall(leftEdgeX, bottomEdgeY, topEdgeY);
+        InstantiateVerticalOuterWall(rightEdgeX, bottomEdgeY, topEdgeY);
+        
+        InstantiateHorizontalOuterWall(leftEdgeX + 1f, rightEdgeX - 1f, bottomEdgeY);
+        InstantiateHorizontalOuterWall(leftEdgeX + 1f, rightEdgeX - 1f, topEdgeY);
+    }
+    
+    void InstantiateVerticalOuterWall(float xCoord, float yStart, float yEnd)
+    {
+        float currentY = yStart;
+        while (currentY <= yEnd)
+        {
+            InstantiateFromArray(outerWallTiles, xCoord, currentY);
+            currentY++;
+        }
+    }
+    void InstantiateHorizontalOuterWall(float xStart, float xEnd, float yCoord)
+    {
+        float currentX = xStart;
+        while (currentX <= xEnd)
+        {
+            InstantiateFromArray(outerWallTiles, currentX, yCoord);
+            currentX++;
+        }
+    }
+    void InstantiateFromArray(GameObject[] prefabs, float xCoord, float yCoord)
+    {
+        //psuedo-random index for the array
+        int randomIndex = Random.Range(0, prefabs.Length);
+        Vector3 position = new Vector3(xCoord, yCoord, 0f);
+        
+        //create an instance of the randomly selected prefab
+        GameObject tileInstance = Instantiate(prefabs[randomIndex], position, Quaternion.identity) as GameObject;
+        
+        tileInstance.transform.parent = boardHolder.transform;
+    }
+}
 //Serializable for the inspector???
 [Serializable]
 public class IntRange
@@ -151,198 +342,6 @@ public class Room
                 break;
         }
     } 
-}
-
-//Game Engine
-public class GameEngine : MonoBehavior {
-    public enum TileType { Wall, Floor }
-    //Room Size
-    public int columns = 100;
-    public int rows = 100;
-    //Range of rooms available per level
-    public IntRange numRooms = new IntRange(15, 20);
-    
-    //Room Size settings
-    public IntRange roomWidth = new IntRange(3, 10);
-    public IntRange roomHeight = new IntRange(3, 10);
-    
-    //Corridor length
-    public IntRange corridorLength = new IntRange(6, 10);
-    
-    //Prefab Objects
-    public GameObject[] floorTiles;
-    public GameObject[] wallTiles;
-    public GameObject[] outerWalls;
-    public GameObject[] NPCs;
-    //....
-    
-    public GameObject player;
-    
-    //Level floor tiles
-    private TileType[][] tiles;
-    //Rooms in the level
-    private Room[] rooms;
-    //Cooridors in the level
-    private Corridor[] corridors;
-    //Container
-    private GameObject levelHolder;
-    
-    //Level Initialization 
-    private void Start() 
-    {
-        levelHolder = new GameObject("Level_1_Holder");
-        SetupTilesArray();
-        CreateRoomsAndCorridors();
-        InstantiateTiles ();
-        InstantiateOuterWalls ();   
-    }
-    
-    void setupTiles()
-    {
-        //needed for level tile grid width
-        tiles = new TileType[columns][];
-        for (int i = 0; i <= tiles.Length; ++i)
-        {
-           //needed for level tile grid height
-           tiles[i] = new TileType[rows];
-        }
-    }
-    void generateRoomsAndCorridors()
-    {
-        //Random number of rooms
-        rooms = new Room[numRooms.Random];
-        //And now we apply some pigeon hole principle for the number of corridors
-        corridors = new Corridor[rooms.Length - 1];
-        
-        //First room of the level
-        rooms[0] = new Room();
-        rooms[0].SetupRoom(roomWidth, roomHeight, columns, rows);
-        corridors[0] = new Corridor();
-        corridors[0].SetupCorridor(rooms[0], corridorLength, roomWidth, roomHeight, columns, rows, true);
-        
-        //generating the rest of the rooms and corridors on the level
-        for(int i = 1; i < rooms.Length; ++i)
-        {
-            rooms[i] = new Room();
-            //Connect room i to corridor i - 1
-            rooms[i].SetupRoom(roomWidth, roomHeight, columns, rows, corridors[i - 1]);
-            
-            //And add a corridor to the new room
-            if (i < corridorLength)
-            {
-                corridors[i] = new Corridor();
-                corridors[i].SetupCorridor(rooms[i], corridorLength, roomWidth, roomHeight, columns, rows, false);    
-            }
-            if(i == rooms.Length *.5f)
-            {
-                Vector3 playerPos = new Vector3 (rooms[i].xPos, rooms[i].yPos, 0);
-                Instantiate(player, playerPos, Quaternion.identity); //??? No idea what this does
-            }
-        }
-    }
-    //tripple nested for loops ftw!!!
-    void SetTileValuesForRooms()
-    {
-        for (int i = 0; i < rooms.Length; ++i)
-        {
-            Room currentRoom = rooms[i];
-            //go through the currentRooms width
-            for (int j = 0; j < currentRoom.roomWidth; ++j)
-            {
-                int xCoord = currentRoom.xPos + j;
-                //for each horizontal tile, go up vertically through the room's height
-                for (int k = 0; k < currentRoom.roomHeight; ++k)
-                {
-                    int yCoord = currentRoom.yPos + k;
-                    tiles[xCoord][yCoord] = TileType.Floor;
-                }
-            }
-        }
-    }
-    void SetTileValuesForCorridors()
-    {
-        for (int i = 0; i < corridors.Length; ++i)
-        {
-            Corridor currentCorridor = corridors[i];
-            //go through the currentCorridors length
-            for(int j = 0; j < currentCorridor.corridorLength; ++j)
-            {
-                int xCoord = currentCorridor.xStartPosition;
-                int yCoord = currentCorridor.yStartPosition;
-              
-                //more direction stuff
-                switch (currentCorridor.direction)
-                {
-                    case Direction.North: yCoord += j; break;
-                    case Direction.East: xCoord += j; break;
-                    case Direction.South: yCoord -= j; break
-                    case Direction.West: xCoord -= j; break;
-                }
-                tiles[xCoord][yCoord] = TileType.Floor;
-            }
-        }
-    }
-    
-    void InstantiateTiles()
-    {
-        //Level floor tiles
-        for (int i = 0; i < tiles.Length; ++i)
-        {
-            for (int j = 0; j < tiles[i].Length; ++j)
-            {
-                InstantiateFromArray(floorTiles, i, j);
-                
-                //Tile Types
-                
-                //Wall
-                if (tiles[i][j] == TileType.Wall) InstantiateFromArray(wallTiles, i, j);
-            }
-        }
-    }
-    void InstantiateOuterWalls()
-    {
-        //Place 1 unit outside the Level Grid
-        float leftEdgeX = -1f;
-        float rightEdgeX = columns + 0f;
-        float bottomEdgeY = -1f;
-        float topEdgeY = rows + 0f;
-        
-        InstantiateVerticalOuterWall(leftEdgeX, bottomEdgeY, topEdgeY);
-        InstantiateVerticalOuterWall(rightEdgeX, bottomEdgeY, topEdgeY);
-        
-        InstantiateHorizontalOuterWall(leftEdgeX + 1f, rightEdgeX - 1f, bottomEdgeY);
-        InstantiateHorizontalOuterWall(leftEdgeX + 1f, rightEdgeX - 1f, topEdgeY);
-    }
-    
-    void InstantiateVerticalOuterWall(float xCoord, float yStart, float yEnd)
-    {
-        float currentY = yStart;
-        while (currentY <= yEnd)
-        {
-            InstantiateFromArray(outerWallTiles, xCoord, currentY);
-            currentY++;
-        }
-    }
-    void InstantiateHorizontalOuterWall(float xStart, float xEnd, float yCoord)
-    {
-        float currentX = xStart;
-        while (currentX <= xEnd)
-        {
-            InstantiateFromArray(outerWallTiles, currentX, yCoord);
-            currentX++;
-        }
-    }
-    void InstantiateFromArray(GameObject[] prefabs, float xCoord, float yCoord)
-    {
-        //psuedo-random index for the array
-        int randomIndex = Random.Range(0, prefabs.Length);
-        Vector3 position = new Vector3(xCoord, yCoord, 0f);
-        
-        //create an instance of the randomly selected prefab
-        GameObject tileInstance = Instantiate(prefabs[randomIndex], position, Quaternion.identity) as GameObject;
-        
-        tileInstance.transform.parent = boardHolder.transform;
-    }
 }
 
 
