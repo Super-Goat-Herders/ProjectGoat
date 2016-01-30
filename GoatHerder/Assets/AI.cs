70333a3a6f558e1da1d27881ffbee7a6388a27f5
Garrett Baski <garbaski@cs.du.edu>
2016-01-30 00:00:10 -0700

    Basic AI lol

---
+++ GoatHerder/Assets/AI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AI : MonoBehaviour {
+    public Transform target;
+    public Vector3 targetPosition;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+        speed = 1f;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+	}
+}
