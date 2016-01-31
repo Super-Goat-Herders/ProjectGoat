af49fb4cc92d5c01f2f6a880bfe916687392d790
Garrett Baski <garbaski@cs.du.edu>
2016-01-31 10:23:14 -0700

    SkeletonAnimationsDone

--- GoatHerder/Assets/SkeletonScript.cs
+++ GoatHerder/Assets/SkeletonScript.cs
@@ -3,16 +3,58 @@ using System.Collections;
 
 public class SkeletonScript : MonoBehaviour {
     public Transform target;
+    Animator playerAnimator;
+    Vector3 lastPostion;
 
 	// Use this for initialization
 	void Start () {
-	
+        playerAnimator = GetComponent<Animator>();
+        lastPostion = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        Vector3 relativePos = target.position - transform.position;
-        Quaternion rotation = Quaternion.LookRotation(relativePos);
-        
+        playerAnimator.SetBool("Attack", true);
+        if (lastPostion == transform.position)
+        {
+            playerAnimator.SetBool("Moving", false);
+        }
+        else
+        {
+            playerAnimator.SetBool("Moving", true);
+        }
+        Vector3 targetRel = target.position - transform.position;
+        if (Mathf.Abs(targetRel.x) > Mathf.Abs(targetRel.y))
+        {
+            if (targetRel.x >= 0)
+            {
+                playerAnimator.SetInteger("Orientation", 1);
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
+            else
+            {
+                playerAnimator.SetInteger("Orientation", 3);
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+            }
+        }
+        else
+        {
+            if (targetRel.y >= 0)
+            {
+                playerAnimator.SetInteger("Orientation", 0);
+            }
+            else
+            {
+                playerAnimator.SetInteger("Orientation", 2);
+            }
+        }
 	}
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Hero")
+        {
+            playerAnimator.SetBool("Attack", true);
+        }
+    }
 }
