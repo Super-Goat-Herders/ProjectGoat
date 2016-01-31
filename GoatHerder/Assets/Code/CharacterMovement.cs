0c57571f4f182cc34bb37853c42ab7ee4e30e63c
Garrett Baski <garbaski@cs.du.edu>
2016-01-30 18:22:18 -0700

    Movement feels ok

--- GoatHerder/Assets/Code/CharacterMovement.cs
+++ GoatHerder/Assets/Code/CharacterMovement.cs
@@ -11,8 +11,14 @@ public class CharacterMovement : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
+    void Update()
+    {
         var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+        playerAnimator.SetBool("Attack", false);
+        if (Input.GetButtonDown("Attack"))
+        {
+            playerAnimator.SetBool("Attack", true);
+        }
         transform.position += move * speed * Time.deltaTime;
         if ((Mathf.Abs(Input.GetAxis("Horizontal")) != 0) || (Mathf.Abs(Input.GetAxis("Vertical")) != 0))
         {
@@ -20,19 +26,21 @@ public class CharacterMovement : MonoBehaviour {
             {
                 playerAnimator.SetBool("Stopped", false);
             }
-            if ((Mathf.Abs(Input.GetAxis("Horizontal")) > (Mathf.Abs(Input.GetAxis("Vertical")))))
+            if ((Mathf.Abs(Input.GetAxis("Horizontal"))) >= (Mathf.Abs(Input.GetAxis("Vertical"))))
             {
                 playerAnimator.SetBool("Forward", false);
                 playerAnimator.SetBool("Backward", false);
                 playerAnimator.SetBool("Sideways", true);
-                if (Input.GetAxis("Horizontal") > 0) {
+                if (Input.GetAxis("Horizontal") > 0)
+                {
                     transform.localRotation = Quaternion.Euler(0, 0, 0);
                 }
-                else{
+                else
+                {
                     transform.localRotation = Quaternion.Euler(0, 180, 0);
                 }
-            
-        }
+
+            }
             else
             {
                 if (Input.GetAxis("Vertical") > 0)
@@ -51,15 +59,16 @@ public class CharacterMovement : MonoBehaviour {
         }
         else
         {
-            playerAnimator.SetBool("Forward", false);
-            playerAnimator.SetBool("Backward", false);
+
+            //playerAnimator.SetBool("Forward", false);
+            //playerAnimator.SetBool("Backward", false);
             playerAnimator.SetBool("Stopped", true);
-            playerAnimator.SetBool("Sideways", false);
+            //playerAnimator.SetBool("Sideways", false);
         }
 
 
         //Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y + 1f, -10f);
-	}
+    }
 
     void OnCollisionEnter2D(Collision2D coll)
     {
