f5cf53db4670d69706aefad52d01d0c551840c9e
Garrett Baski <garbaski@cs.du.edu>
2016-01-30 23:22:13 -0700

    Nathan Do Math for Cardinal Direction of Skeleton
    Nathan do this shit

---
+++ GoatHerder/Assets/SkeletonScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SkeletonScript : MonoBehaviour {
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 relativePos = target.position - transform.position;
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
+        
+	}
+}
